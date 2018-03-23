4e371b4889026b100486e4b0249faacdec2e7cf3
lichen545 <lichen545@gmail.com>
2018-03-23 03:50:28 -0500

    Finished all functions

--- transactionProcessor.java
+++ transactionProcessor.java
@@ -30,9 +30,9 @@ class transactionProcessor {
 	}
 
 	public static void average(Statement stmt) throws SQLException {
-		System.out.println("averaging all prices...");
+		System.out.println("averaging all prices:");
 		ResultSet rs = stmt.executeQuery("SELECT avg(price) FROM parts");
-		System.out.println(rs.getInt(1));
+		System.out.println(">>>" + rs.getInt(1));
 	}
 
 	public static void all_subparts(Statement stmt, String[] args) throws SQLException {
@@ -57,14 +57,44 @@ class transactionProcessor {
 	}
 
 	public static void subpart_average(Statement stmt, String[] args) throws SQLException {
-		// ResultSet rs = stmt.executeQuery("select * from parts");
-		System.out.println("averaging prices of all subparts...");
+		System.out.println("averaging prices of all subparts of pid = " + args[1]);
+		ResultSet rs = stmt.executeQuery("SELECT * FROM subpart_of WHERE mid = " + args[1]);
+		List<Integer> subparts = new ArrayList<Integer>();
+		while (rs.next()) {
+			subparts.add(rs.getInt(1));
+		}
 
+		//search for subparts of subparts
+		for (int i=0; i<subparts.size(); i++) {
+			ResultSet ss = stmt.executeQuery("SELECT * FROM subpart_of WHERE mid = " + subparts.get(i));
+			while (ss.next()) {
+				if (!subparts.contains(ss.getInt(1))) {
+					subparts.add(ss.getInt(1));
+				}
+			}
+		}
+
+		//find average price and print:
+		Integer sum = 0;
+		for (Integer pid : subparts){
+			rs = stmt.executeQuery("SELECT * FROM parts WHERE pid = " + pid.toString());
+			sum += rs.getInt(3);
+		}
+		
+		System.out.println(">>>avg price of subparts " + subparts.toString() + ": " + (sum/subparts.size()));
 	}
 
 	public static void is_multiple_subpart(Statement stmt, String[] args) throws SQLException {
-		// ResultSet rs = stmt.executeQuery("select * from parts");
-		System.out.println("finding all parts that are subparts of multiple parts...");
+		System.out.println("finding all parts that are subparts of multiple parts:");
+		ResultSet rs = stmt.executeQuery("SELECT name " +
+										 "FROM parts JOIN 	(SELECT pid, count(pid) as count " +
+						 								  	"FROM subpart_of " +
+						 									"GROUP BY pid) as multiple " +
+										 "ON parts.pid = multiple.pid " +
+										 "WHERE count>1");
+		while (rs.next()) {
+			System.out.println(">>>" + rs.getString("name"));
+		}
 	}
 
 	public static void main(String args[]) throws ClassNotFoundException {
@@ -84,15 +114,6 @@ class transactionProcessor {
 			stmt.executeUpdate("create table parts (pid integer, name string, price integer, PRIMARY KEY(pid))");
 			stmt.executeUpdate("create table subpart_of (pid integer, mid integer)");
 
-			//test values
-			// stmt.executeUpdate("insert into parts values(1, 'nut', 10)");
-			// stmt.executeUpdate("insert into parts values(2, 'tire', 200)");
-			// stmt.executeUpdate("insert into parts values(3, 'rim', 100)");
-			// stmt.executeUpdate("insert into parts values(4, 'wheel', 340)");
-			// ResultSet rs = stmt.executeQuery("select * from parts");
-			// while (rs.next())
-			// 	System.out.println(rs.getInt(1) + "  " + rs.getString(2));
-
 			//read file input
 			try (BufferedReader br = new BufferedReader(new FileReader("transfile.txt"))) {
 				String line = br.readLine();
@@ -101,7 +122,7 @@ class transactionProcessor {
 
 					String[] arguments = line.split(" ");
 					System.out.println("\nArguments:" + Arrays.toString(arguments));
-					System.out.println("--Argument length: " + arguments.length);
+					// System.out.println("--Argument length: " + arguments.length);
 					// System.out.println(arguments[0].getClass().getSimpleName());
 
 					if (arguments[0].equals("1")) {
@@ -123,8 +144,8 @@ class transactionProcessor {
 			}
 
 			//clear tables
-			// stmt.executeUpdate("drop table if exists parts");
-			// stmt.executeUpdate("drop table if exists subpart_of");
+			stmt.executeUpdate("drop table if exists parts");
+			stmt.executeUpdate("drop table if exists subpart_of");
 
 			con.close();
 
